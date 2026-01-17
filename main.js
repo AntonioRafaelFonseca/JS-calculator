0b7a4eaed455b2462d553988d5541625e82e38d3
AntonioRafaelFonseca <rafamarim19@gmail.com>
2026-01-17 22:39:08 +0000

    honestly idk what I did but it's done

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const display = document.getElementById('display')
+
+let answ;
 let solved = false;
 
 function append(n) {
@@ -9,20 +11,40 @@ function append(n) {
   display.value += n;
   
 }
+function saveNum(){
+  answ = display.value
+  clearDisp()
+}
 function clearDisp() {
   display.value = ""
 }
 function result() {
   try{
-    display.value = round(eval(display.value), 5);
+    v = round(eval(display.value), 5)
+    if(Number.isNaN(v)){
+      throw new Error('Write Smthng bruh')
+    }
+    display.value = v;
   }
   catch(err){
-    display.value = "are you f** dumb???"
+    display.value = "are you dumb???"
   }
-  
   solved = true;
 }
+
 function round(number, decimals=0){
   const factor = Math.pow(10, decimals);
   return Math.round(number * factor) / factor;
 }
+document.addEventListener('keydown', event => {
+  if(event.code.includes('Digit') || ['+', '-', '*', '/'].includes(event.key)){
+    append(event.key)
+    console.log('numPressed')
+  }else if(event.key == 'c'){
+    clearDisp()
+  }else if(event.key == 'Enter'){
+    event.preventDefault()
+    result()
+  }
+  console.log(event)
+})
